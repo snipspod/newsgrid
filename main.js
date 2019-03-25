48cff66b1e415b6ff35f8ebebbb2fb5f6df04b61
snipspod <jast99@gmail.com>
2019-03-25 16:18:41 +0100

    Version 1.01

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let prevScrollpos = window.pageYOffset;
-window.addEventListener('scroll', function(){
-  let currentScrollpos = window.pageYOffset;
-  if(prevScrollpos > currentScrollpos){
-    document.querySelector('#main-nav').style.top = "0";
-  }else{
-    document.querySelector('#main-nav').style.top = "-120px";
-  }
-  prevScrollpos = currentScrollpos;
-})
+if (window.matchMedia("(max-width: 768px)").matches) {
+  let prevScrollpos = window.pageYOffset;
+  window.addEventListener("scroll", function() {
+    let currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+      document.querySelector("#main-nav").style.top = "0";
+    } else {
+      document.querySelector("#main-nav").style.top = "-120px";
+    }
+    prevScrollpos = currentScrollpos;
+  });
+}
