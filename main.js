e1021a2304ceb6e74e421fa033744b6300ec1496
snipspod <jast99@gmail.com>
2019-03-25 16:13:07 +0100

    Version 1.01

---
+++ main.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', function(){
+  let currentScrollpos = window.pageYOffset;
+  if(prevScrollpos > currentScrollpos){
+    document.querySelector('#main-nav').style.top = "0";
+  }else{
+    document.querySelector('#main-nav').style.top = "-120px";
+  }
+  prevScrollpos = currentScrollpos;
+})
